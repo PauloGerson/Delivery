a0af6feb53b1db2d6dd49e25df1612e5cee85a0f
PauloGerson <paulogerson81@gmail.com>
2021-06-24 10:32:31 -0300

    update responsivo
    atualizando o responsivo

--- script/main.js
+++ script/main.js
@@ -1,7 +1,17 @@
-const btnMobile = document.querySelector('.btn-mobile');
+const btnMobile = document.getElementById('btn-mobile');
 
-function toggleMenu(){
-    const nav = document.querySelector('.topo');
-    nav.classList.toggle('active');
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
 }
+
 btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
